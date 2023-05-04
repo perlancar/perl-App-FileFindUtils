09dba9636d05e86129cbc5d5dc0f8c05bc471bec
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-05-04 22:29:20 +0700

    Initial commit

---
+++ lib/App/FileFindUtils.pm
@@ -0,0 +1,23 @@
+package App::FileFindUtils;
+
+use strict;
+use warnings;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Utilities related to finding files
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
