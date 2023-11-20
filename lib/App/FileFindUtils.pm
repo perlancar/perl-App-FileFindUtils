ba2d4f60aac14571c2b40985d0815a29bda221aa
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-20 16:09:42 +0700

    Remove all the scripts because they are now in App-FileSortUtils, add find_wanted

--- lib/App/FileFindUtils.pm
+++ lib/App/FileFindUtils.pm
@@ -8,6 +8,31 @@ use warnings;
 # DIST
 # VERSION
 
+our %SPEC;
+
+$SPEC{find_wanted} = {
+    v => 1.1,
+    summary => 'Find files based on some criteria (CLI front-end for File::Find::Wanted)',
+    args => {
+        wanted => {
+            schema => 'code_from_str*',
+            req => 1,
+            pos => 0,
+        },
+        dirs => {
+            schema => ['array*', of=>'dirname*'],
+            default => ['.'],
+        },
+    },
+    result_naked => 1,
+};
+sub find_wanted {
+    my %args = @_;
+
+    require File::Find::Wanted;
+    File::Find::Wanted::find_wanted($args{wanted}, @{ $args{dirs} });
+}
+
 1;
 #ABSTRACT: Utilities related to finding files
 
